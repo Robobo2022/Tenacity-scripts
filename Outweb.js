4bdd33bb7ba7df43e42b8b031c57d37f1bdc6024
Bo <44844728+Robobo2022@users.noreply.github.com>
2022-11-22 12:31:01 +0100

    Update Outweb.js

--- Outweb.js
+++ Outweb.js
@@ -10,6 +10,5 @@ var Outweb = script.booleanSetting("Outweb Toggle", false)
 script.onTick(function () {
     if (Outweb.isEnabled() == true && player.inWeb()) {
         player.setSpeed(7)
-        player.setMotionY(0.10)
     }
 })
